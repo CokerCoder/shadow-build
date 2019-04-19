3b77c68d3079042efd5d8bf8a30592a07dcf93e9
jingy277 <jingy277@gmail.com>
2019-04-19 10:51:03 +1000

    Add files via upload

---
+++ src/Camera.java
@@ -0,0 +1,77 @@
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.tiled.TiledMap;
+
+/**
+ * This class should be used to restrict the game's view to a subset of the entire world.
+**/
+
+public class Camera {
+	
+	// Amount of pixels need to be transfered when renedering the map
+	private float transX, transY;
+	// Size of the map used to check if out of bound
+	private float mapWidth, mapHeight;
+   
+	// Camera position in a World coordinates
+	// Define a rectangle with the size of our window, this represents our camera "range"
+    // So everything inside the viewport will be drawn on the screen
+	// The camera starting position is its top-left corner
+	private Rectangle viewPort; 
+ 
+	// Camera constructor
+	public Camera(TiledMap map, int mapWidth, int mapHeight) {
+		transX = 0;
+		transY = 0;
+      
+		// The initial window is the top left corner of the whole map which starts at (0, 0)
+		viewPort = new Rectangle(0, 0, App.WINDOW_WIDTH, App.WINDOW_HEIGHT);
+      
+		this.mapWidth = mapWidth;
+		this.mapHeight = mapHeight;
+	}
+	
+	// Calculate the movement along the x and y axis
+	public void translate (Graphics g, Sprite player) {
+		
+		/* The number 16 means the number of pixels of the map
+		 * that we need to let the render method to render this much more
+		 * which will look more smooth when the player is about to the edge
+		 * I don't know if this count as a constant becasue everyone has different
+		 * definition for what is smooth. However I do think 16 fits the best
+		 */
+		
+		// If the player is at left edge of the map, keep the camera at left so the player cannot go across the boundry
+		if(player.getPos().x-App.WINDOW_WIDTH/2+16 < 0)
+			transX = 0;
+		// ...right edge
+		else if(player.getPos().x+App.WINDOW_WIDTH/2+16 > mapWidth)
+			transX = -mapWidth+App.WINDOW_WIDTH;
+		// Otherwise update the movement
+		else
+			transX = -player.getPos().x+App.WINDOW_WIDTH/2-16;
+		
+		// Vise versa
+		if(player.getPos().y-App.WINDOW_HEIGHT/2+16 < 0)
+			transY = 0;
+		else if(player.getPos().y+App.WINDOW_HEIGHT/2+16 > mapHeight)
+			transY = -mapHeight+App.WINDOW_HEIGHT;
+		else
+			transY = -player.getPos().y+App.WINDOW_HEIGHT/2-16;
+		
+		// Apply the transformation and update the camera location
+		g.translate(transX, transY);
+		viewPort.setX(-transX);
+		viewPort.setY(-transY);
+		
+	}
+	
+	// Hepler method to calculate a point position in a World perpective (the total map)
+	// based on its window position and the camera position in the World
+    public float calcWorldX(float cameraX) {
+		return cameraX+viewPort.getX();
+	}
+	public float calcWorldY(float cameraY) {
+		return cameraY+viewPort.getY();
+	}
+}
---
+++ src/World.java
@@ -0,0 +1,71 @@
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.tiled.TiledMap;
+
+/**
+ * This class should be used to contain all the different objects in your game world, and schedule their interactions.
+ * 
+ * You are free to make ANY modifications you see fit.
+ * These classes are provided simply as a starting point. You are not strictly required to use them.
+ */
+
+public class World {
+	
+	// Constants
+	public static final int LAYER_INDEX = 0;
+	public static final float MIN_DISTANCE = 0.25f;
+	
+	public static final float PLAYER_SPEED = 0.25f;
+	
+	// Set the local variables for the map, player and camera
+	private final TiledMap map;
+	private final Sprite player;
+	private final Camera camera;
+	
+	// Size of the map in pixels
+	private int mapWidth, mapHeight;
+	
+	// Get the destination position and store it into a vector
+	private Vector2f dest = new Vector2f(0, 0);
+
+
+	// Construct the World
+	public World() throws SlickException {
+		map = new TiledMap("assets/main.tmx");
+		mapWidth = map.getWidth() * map.getTileWidth();
+		mapHeight = map.getHeight() * map.getTileHeight();
+		
+		// Initialise the player at the same position as the destination position
+		// with a speed of 0.25
+		player = new Player(dest.x, dest.y, map, new Image("assets/scout.png"), PLAYER_SPEED);
+		// Initialise the camera
+		camera = new Camera(map, mapWidth, mapHeight);
+	}
+	
+	public void update(Input input, int delta) {
+		if(input.isMousePressed(Input.MOUSE_RIGHT_BUTTON)) {
+			// Calculate the mouse position respect to the world using the function in the Camera class
+			dest.x = camera.calcWorldX(input.getMouseX());
+			dest.y = camera.calcWorldY(input.getMouseY());
+		}
+		// Update the player's position
+		player.update(delta, dest);
+	}
+	
+	public void render(Graphics g) {
+		// Firstly translate the camera based on the player's position
+		camera.translate(g, player);
+		// Display the map onto the screen
+		map.render(0, 0);
+		player.render(g);
+	}
+	
+
+	// Helper method to calculate the angle that between two points
+	public static double getAngle(float x1, float y1, float x2, float y2) {
+		return (Math.atan2(y2-y1, x2-x1));
+	}
+}
